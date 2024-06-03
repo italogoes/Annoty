f089285f351151be42b757da855fc4ff688ca0d4
Italo Goes <italogoes85@gmail.com>
2024-06-03 00:45:29 -0300

    feat: express validator

--- backend/src/routes/Routes.ts
+++ backend/src/routes/Routes.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import User from "../controllers/UserController";
 const routes = express.Router();
+// Controllers
+import User from "../controllers/UserController";
+// Validators
+import validate from "../middlewares/HandleValidation";
+const { userCreateValidator } = require("../middlewares/UserValidation")
+
 
 routes.get("/api/users", User.getUser)
-routes.post("/api/register", User.addUser)
+routes.post("/api/register", validate, userCreateValidator(), User.addUser)
 
 export default routes
