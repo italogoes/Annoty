9a9ed9e0876137bee078d7c480c5d0f5d6df8575
Italo Goes <italogoes85@gmail.com>
2024-06-08 01:24:27 -0300

    feat: login de usuario completo

--- backend/src/controllers/ProjectController.ts
+++ backend/src/controllers/ProjectController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 
 class ProjectController {
     public async getProjects(req: Request, res: Response){
-        
+        // ver amanha
+        // https://dev.to/jhonywalkeer/nodejs-limitacao-de-taxa-de-api-6n3
     }
 }
 
